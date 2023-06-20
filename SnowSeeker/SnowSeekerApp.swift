a2394a9855545b4e2cc8450069af751a9884b286
EricTMcode <93444335+EricTMcode@users.noreply.github.com>
2023-06-20 13:54:04 +0200

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Eric on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
